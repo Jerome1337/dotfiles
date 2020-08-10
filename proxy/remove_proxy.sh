2b6795a9c2f38578bcc2b0e62a62c237857938a7
Jérôme Pogeant <jerome.pogeant@atos.net>
2020-08-10 11:09:05 +0200

    Fixes remove/setup proxy script

--- proxy/remove_proxy.sh
+++ proxy/remove_proxy.sh
@@ -4,11 +4,16 @@ git config --global --unset http.proxy
 git config --global --unset https.proxy
 
 if [[ -f /etc/systemd/system/docker.service.d/http-proxy.conf ]]; then
-   sudo sed -i '/Environment="HTTP_PROXY=/d' /etc/systemd/system/docker.service.d/http-proxy.conf
+    sudo sed -i '/Environment="HTTP_PROXY=/d' /etc/systemd/system/docker.service.d/http-proxy.conf
+
+    sudo systemctl daemon-reload
+    sudo systemctl restart docker.service
 fi
 
-unset {http,https,ftp}_proxy
+unset http_proxy https_proxy ftp_proxy HTTP_PROXY HTTPS_PROXY
 
 npm config delete proxy
 
-sudo sed -i '/Acquire::http::Proxy/ s/"[^"][^"]*"/"false"/' /etc/apt/apt.conf
+if [[ -f /etc/apt/apt.conf ]]; then
+    sudo sed -i 's/"[^"][^"]*"/"false"/' /etc/apt/apt.conf
+fi
--- proxy/setup_proxy.sh
+++ proxy/setup_proxy.sh
@@ -2,21 +2,31 @@
 
 if [ -z "$1" ]; then
     echo "No proxy supplied"
-    
+
     exit 1
 fi
 
-# Configure Git to use proxy
-git config --global http.proxy "http://$1/"
-git config --global https.proxy "http://$1/"
+PROXY_HTTP_URL="http://$1/"
+PROXY_HTTPS_URL="http://$1/"
+
+git config --global http.proxy $PROXY_HTTP_URL
+git config --global https.proxy $PROXY_HTTPS_URL
 
 if [[ -f /etc/systemd/system/docker.service.d/http-proxy.conf ]]; then
-   sudo sed -i '/Environment="HTTP_PROXY=/d' /etc/systemd/system/docker.service.d/http-proxy.conf
+    sudo sed -i '/Environment="HTTP_PROXY=/d' /etc/systemd/system/docker.service.d/http-proxy.conf
+
+    sudo systemctl daemon-reload
+    sudo systemctl restart docker.service
 fi
 
-export {http,https,ftp}_proxy="http://$1/"
+export http_proxy=$PROXY_HTTP_URL
+export https_proxy=$PROXY_HTTPS_URL
+export ftp_proxy=$PROXY_HTTP_URL
+export HTTP_PROXY=$PROXY_HTTP_URL
+export HTTPS_PROXY=$PROXY_HTTPS_URL
 
-# Configure APT to use proxy
-echo `Acquire::http::Proxy "http://$1";` | sudo tee -a /etc/apt/apt.conf
+if [[ -f /etc/apt/apt.conf ]]; then
+    sudo sed -i "s@false@$PROXY_HTTP_URL@" /etc/apt/apt.conf
+fi
 
-npm config set proxy http://$1
+npm config set proxy $PROXY_HTTP_URL
